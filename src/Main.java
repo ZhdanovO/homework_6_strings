9a1f7101636e910e0cb127db4b238d0b103be656
ZhdanovO <oleg.zhdanov88@gmail.com>
2022-06-29 12:36:48 +0300

    First part of homework

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        //task 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника " + fullName);
 
+        //task 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //task 3
+        System.out.println("Данные ФИО сотрудника для административного отдела — " + fullName.replace(" ", "; "));
+
+        //task 4
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace("ё", "е"));
     }
 }
